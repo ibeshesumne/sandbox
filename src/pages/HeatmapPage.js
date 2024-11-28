617bb4cd299a0ba915ad1eb2f18864c505d819db
ibeshesumne <ml@mleary.idv.hk>
2024-11-28 20:24:41 +0000

    horizontal scrollable svg heatmap for mobile

--- src/pages/HeatmapPage.js
+++ src/pages/HeatmapPage.js
@@ -5,7 +5,7 @@ const HeatmapPage = () => {
   return (
     <div>
       <h1 className="text-center text-2xl font-bold my-4">
-        Frequency of Letters Exchanged by Henri Thomas (2005–2018)
+        Frequency of Letters Exchanged by Henri Thomas (1930-1997)
       </h1>
       <p className="text-center text-gray-600 mb-6">
         Visualizing correspondence patterns from archival data.
